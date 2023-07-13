27d39afa5789958ee21ce3405c68324e010d22c1
Kyle Clapper <35474281+kclapper@users.noreply.github.com>
2023-07-13 11:26:18 -0400

    UI simplification and responsiveness (#2)
    * Counter improvements
    
    * Simplify UI design and make more responsive
    
    The UI has been simplified a bit. Zoom functionality is now
    accomplished using the browswer CMD-+ keyboard shortcut.
    
    The client details and counter number will now automatically grow to
    accomodate text size.
    
    * Build simplified version and release v0.2.0

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import { useEffect, useCallback } from 'react';
+
 export function setMinimum(fn, min=1) {
   return (arg) => {
     if (arg < min) {
@@ -5,4 +7,36 @@ export function setMinimum(fn, min=1) {
     }
     fn(arg);
   }
+}
+
+export function useKeyHandler(keyHandler) {
+  const handler = useCallback(keyHandler, [keyHandler]);
+    useEffect(() => {
+        document.addEventListener('keydown', handler);
+        return () => {
+            document.removeEventListener('keydown', handler);
+        };
+    }, [handler]);
+}
+
+export function calcTextWidth(str, min=0) {
+  if (typeof min !== 'number') {
+    min = String(min).length;
+  }
+  str = String(str);
+
+  const letterFactor = 0.68;
+  const numberFactor = 1;
+
+  let acc = 0;
+  for (let i = 0; i < str.length; i++) {
+    const charCode = str.charCodeAt(i);
+    if (48 <= charCode && charCode <= 57) {
+      acc += numberFactor;
+    } else {
+      acc += letterFactor;
+    }
+  }
+
+  return Math.max(acc, min) + "ch";
 }
